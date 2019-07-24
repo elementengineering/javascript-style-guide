0628915843a87ef15f580afad6f6345fab07af08
Blake Lovelace <lovelace.b@hotmail.com>
2019-07-24 13:32:19 +0800

    Update config to include sorting of imports and keys

--- linters/reactjs/index.js
+++ linters/reactjs/index.js
@@ -6,6 +6,7 @@ module.exports = {
         node: true
     },
     rules: {
+        'comma-dangle': 1,
         indent: [
             2,
             4,
@@ -15,35 +16,48 @@ module.exports = {
                 'outerIIFEBody': 1
             }
         ],
-        'react/jsx-indent': [0],
-        'react/jsx-indent-props': [0],
-        'max-len': [1, 120],
-        'comma-dangle': 1,
-        'no-param-reassign': 1,
-        'no-console': [
-            1,
-            { 'allow': ['warn', 'error'] }
-        ],
-        'react/no-deprecated': 1,
         'jsx-a11y/anchor-is-valid': [
             'error',
             {
+                aspects: ['noHref', 'invalidHref', 'preferButton'],
                 components: ['Link'],
-                specialLink: ['to'],
-                aspects: ['noHref', 'invalidHref', 'preferButton']
+                specialLink: ['to']
             }
         ],
         'linebreak-style': 0,
+        'max-len': [1, 120],
+        'no-console': [
+            1,
+            { 'allow': ['error', 'warn'] }
+        ],
+        'no-param-reassign': 1,
         'no-plusplus': [
             'error',
             { 'allowForLoopAfterthoughts': true }
         ],
         'radix': ['error', 'as-needed'],
+        'react/jsx-first-prop-new-line': 0,
+        'react/jsx-indent': [0],
+        'react/jsx-indent-props': [0],
         'react/jsx-max-props-per-line': [
             'error',
             { 'maximum': 2, 'when': 'multiline' }
         ],
-        'react/jsx-first-prop-new-line': 0
+        'react/no-deprecated': 1,
+        'sort-keys': [
+            "error",
+            "asc",
+            {
+                "caseSensitive": false
+            }
+        ],
+        'sort-imports': [
+            "error",
+            {
+                "ignoreCase": true,
+                "ignoreDeclarationSort": true
+            }
+        ]
     },
     settings: {
         react: {
